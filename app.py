61dfd3a0ca438766463557e88604e3a37af035f5
Ron Lai (MacBook AIR) <laironald@gmail.com>
2013-08-15 14:18:45 -0700

    generate a new file output

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
+import csv
 import glob
 import sys
 import lib
 import os
+from datetime import datetime
 
 
 config = lib.get_config()
+
+
+def reformat_data(header, row):
+    data = {}
+    print header
+    print row.split("\t")
+
+    for i, item in enumerate(row.split("\t")):
+        if item[-5:] == "+0000":
+            data[header[i]] = datetime.strptime(item, '%Y-%m-%d %H:%M:%S +0000')
+        if item[-3:] == "UTC":
+            data[header[i]] = datetime.strptime(item, '%Y-%m-%d %H:%M:%S UTC')
+
+    print data
+    print ""
 
 
 def data_clean(filename):
@@ -13,7 +30,18 @@ def data_clean(filename):
         if os.path.exists(outfile):
             continue
         infile = open(infile, "rb")
-        outfile = open(outfile, "wb")
+        outfile = csv.writer(open(outfile, "wb"))
+
+        header = None
+        for row in infile:
+            row = row.replace("\n", "")  # remove newlines
+            if not header:
+                if row.count('\\t') > 10:  # this is likely the header
+                    header = row.split('\\t')
+                    outfile.writerow(header)
+            else:
+                outfile.writerow(row.split("\t"))
+                # reformat_data(header, row)
 
 
 if __name__ == '__main__':

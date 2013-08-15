3ac0fd7464cd4dbca13461bb07735162f56b59a5
Ron Lai (MacBook AIR) <laironald@gmail.com>
2013-08-15 13:44:59 -0700

    input / output setting for a file

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import glob
 import sys
 import lib
+import os
 
 
 config = lib.get_config()
 
 
 def data_clean(filename):
-    print filename
-    pass
+    for infile in filename:
+        outfile = "{file}-{processed}".format(file=infile, processed=config.get("input").get("processed"))
+        if os.path.exists(outfile):
+            continue
+        infile = open(infile, "rb")
+        outfile = open(outfile, "wb")
 
 
 if __name__ == '__main__':
